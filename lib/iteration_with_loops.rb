1e30de3647720107e6c3329075585aab0fe20957
Daniel Cooper <cooperdaniel5141@gmail.com>
2020-05-20 17:18:25 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -8,10 +8,11 @@ idx = 0
 while idx < src.length do
   inner_idx = 0
   while inner_idx < src[idx].length do
-    if src[idx][inner_idx].even
+    if src[idx][inner_idx].even?
       puts src[idx][inner_idx]
     end
     inner_idx += 1
   end
   idx =+ 1
+end
 end
